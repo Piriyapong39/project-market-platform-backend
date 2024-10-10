09cac06af47d6bc9853ad6e8c185783c1bb3de1a
Piriyapong <virote3011@gmail.com>
2024-10-10 15:39:28 +0700

    Loop picture file

--- services/remove-files.js
+++ services/remove-files.js
@@ -3,20 +3,20 @@ class RemoveFile {
     constructor(){}
     removePic(picPathsArr){
         try {
-            const nameSplit = picPathsArr[0].split("\\")
-            const picName = nameSplit[nameSplit.length-1]
-            const extName = picName.split(".")[1]
-            if(extName === "png" || extName === "jpeg" || extName === "jpg"){
-                picPathsArr.forEach(e => {
+            picPathsArr.forEach(e => {
+                const nameSplit = e.split("\\")
+                const picName = nameSplit[nameSplit.length - 1]
+                const extName = picName.split(".")[1].toLowerCase()
+                if(extName === "png" || extName === "jpeg" || extName === "jpg"){
                     fs.rm(e, {recursive: true}, (error) => { 
                         if(error){ 
                             throw new Error(error)
                         } 
                     }) 
-                })
-            } else {
-                throw new Error("Wrong extname to remove")
-            }
+                }else{
+                    throw new Error("wrong extname to remove")
+                }
+            })
         } catch (error) {
             throw error
         }
